f3532323a5766451c99c4177eef4642499a545a4
hungntth <1119thaihungnguyen@gmail.com>
2025-03-04 22:09:05 +0700

    fix create order

--- order_service/src/service/order.service.ts
+++ order_service/src/service/order.service.ts
@@ -10,48 +10,45 @@ export const CreateOrder = async (
   repo: OrderRepositoryType,
   cartRepo: CartRepositoryType
 ) => {
-  // find cart by customer id
-  const cart = await cartRepo.findCart(userId);
-  if (!cart) {
-    throw new Error("Cart not found");
-  }
-  // calculate total ordre amount
-  let cartTotal = 0;
-  let orderLineItems: OrderLineItemType[] = [];
-
-  // create orderline items from cart items
-  cart.lineItems.forEach((item) => {
-    cartTotal += item.qty * Number(item.price);
-    orderLineItems.push({
-      productId: item.productId,
-      itemName: item.itemName,
-      qty: item.qty,
-      price: item.price,
-    } as OrderLineItemType);
-  });
-
-  const orderNumber = Math.floor(Math.random() * 1000000);
-
-  // create order with line items
-  const orderInput: OrderWithLineItems = {
-    orderNumber: orderNumber,
-    txnId: null,
-    status: OrderStatus.PENDING,
-    customerId: userId,
-    amount: cartTotal.toString(),
-    orderItems: orderLineItems,
-  };
-
-  // const order = await repo.createOrder(orderInput);
-  // await cartRepo.clearCartData(userId);
-  // console.log("Order created", order);
-  // fire a message to subscription service [catalog service] to update stock
-  // await repo.publishOrderEvent(order, "ORDER_CREATED");
-
-  await SendCreateOrderMessage(orderInput);
-
-  // return success message
-  return { message: "Order created successfully", orderNumber: orderNumber };
+   // find cart by customer id
+   const cart = await cartRepo.findCart(userId);
+   if (!cart) {
+     throw new Error("Cart not found");
+   }
+   // calculate total ordre amount
+   let cartTotal = 0;
+   let orderLineItems: OrderLineItemType[] = [];
+ 
+   // create orderline items from cart items
+   cart.lineItems.forEach((item) => {
+     cartTotal += item.qty * Number(item.price);
+     orderLineItems.push({
+       productId: item.productId,
+       itemName: item.itemName,
+       qty: item.qty,
+       price: item.price,
+     } as OrderLineItemType);
+   });
+ 
+   const orderNumber = Math.floor(Math.random() * 1000000);
+ 
+   // create order with line items
+   const orderInput: OrderWithLineItems = {
+     orderNumber: orderNumber,
+     txnId: null, // Payment ID to keep track of successful payment status
+     status: OrderStatus.PENDING, // payment status will be updated by payment service
+     customerId: userId,
+     amount: cartTotal.toString(),
+     orderItems: orderLineItems,
+   };
+ 
+   const order = await repo.createOrder(orderInput);
+   await cartRepo.clearCartData(userId);
+   console.log("Order created", order);
+   // fire a message to subscription service [catalog service] to update stock
+   await SendCreateOrderMessage(orderInput);
+   // return success message
+   return { message: "Order created successfully", orderNumber: orderNumber };
 };
 
 export const UpdateOrder = async (
